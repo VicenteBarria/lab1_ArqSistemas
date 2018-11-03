d6e02f171b292107cc1f0812989fada2076f103e
vicentebarria <v.barriaveas@gmail.com>
2018-11-03 20:36:08 -0300

    changes

--- bat_files/buildContainer.bat
+++ bat_files/buildContainer.bat
@@ -1,3 +1,3 @@
 cd django-ribbit
-docker-compose up
+docker-compose up -d
 cd ..
--- bat_files/pushToHeroku.bat
+++ bat_files/pushToHeroku.bat
@@ -1,7 +1,8 @@
 cd django-ribbit
 docker login
 heroku login
-heroku container: push web
+heroku container:login
+heroku container:push web
 heroku container:release web
 heroku open
 cd ..
