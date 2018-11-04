dec2c82518e2ebbcc79e32d1eca2fef508b40075
vicentebarria <v.barriaveas@gmail.com>
2018-11-04 20:04:57 -0300

    still fighting with Heroku

--- bat_files/pushToHeroku.bat
+++ bat_files/pushToHeroku.bat
@@ -1,7 +1,8 @@
 cd NotMyJob
+cd django-ribbit
 heroku login
 heroku container:login
-heroku container:push web
-heroku container:release web
-heroku open
+heroku container:push web --app django-ribbit
+heroku container:release web --ap django-ribbit
+heroku open --app django-ribbit
 cd ..
