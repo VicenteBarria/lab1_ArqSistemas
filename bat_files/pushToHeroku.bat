d2b68e4d49535d8c5181c71a2c0c305fa422fd69
vicentebarria <v.barriaveas@gmail.com>
2018-11-04 22:09:29 -0300

    fixed a typo

--- bat_files/pushToHeroku.bat
+++ bat_files/pushToHeroku.bat
@@ -3,6 +3,6 @@ cd django-ribbit
 heroku login
 heroku container:login
 heroku container:push web --app django-ribbit
-heroku container:release web --ap django-ribbit
+heroku container:release web --app django-ribbit
 heroku open --app django-ribbit
 cd ..
